17566dcd2bf1b85d8817eeb490f86b2f4ecc8192
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2019-10-31 21:44:31 +0000

    add more path to setup scripts

--- setup/setup_UCVMC.sh
+++ setup/setup_UCVMC.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+## base models are cvmh1511/cvmh-15.1.1.tar.gz/CVM-Hv15.1.1 
+##             and cvmsi/cvms426.tar.gz/CVM-S4.26.M01
+
 export TOP_UCVM_DIR=`pwd`
 
 export ANACONDA2=/usr/local/share/anaconda2
@@ -8,9 +12,9 @@ export UCVM_SRC_PATH=$TOP_UCVM_DIR/model/UCVMC
 export UCVM_INSTALL_PATH=$TOP_UCVM_DIR/model/UCVMC_TARGET
 
 if [ $LD_LIBRARY_PATH ] ; then
-  export LD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib:$UCVM_INSTALL_PATH/model/cvmh1511/lib:$UCVM_INSTALL_PATH/model/cvmsi/lib:$LD_LIBRARY_PATH
   else
-    export LD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib
+    export LD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib:$UCVM_INSTALL_PATH/model/cvmh1511/lib:$UCVM_INSTALL_PATH/model/cvmsi/lib
 fi
 
 ## setup python script
@@ -20,7 +24,7 @@ if [ $PYTHONPATH ] ; then
      export PYTHONPATH=$UCVM_INSTALL_PATH/utilities/pycvm
 fi
 
-export DYLD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib
+export DYLD_LIBRARY_PATH=$UCVM_INSTALL_PATH/lib/euclid3/lib:$UCVM_INSTALL_PATH/lib/proj-5/lib:$UCVM_INSTALL_PATH/model/cvmh1511/lib:$UCVM_INSTALL_PATH/model/cvmsi/lib
 
 if [ $PATH ] ; then
   export PATH=$UCVM_INSTALL_PATH/bin:$PATH
--- setup/setup_UCVM_web.sh
+++ setup/setup_UCVM_web.sh
@@ -7,16 +7,12 @@ export ANACONDA2=/usr/local/share/anaconda2
 
 export UCVM_SRC_PATH=$TOP_DIR/model/UCVMC
 export UCVM_INSTALL_PATH=$TOP_DIR/model/UCVMC_TARGET
-
-chmod og+rw $TOP_UCVM_WEB_DIR/web/result
-ln -s $UCVM_INSTALL_PATH $TOP_UCVM_WEB_DIR/model
-
 export UCVM_WEB_INSTALL_PATH=$TOP_UCVM_WEB_DIR/model/UCVMC_TARGET
 
 if [ $LD_LIBRARY_PATH ] ; then
-  export LD_LIBRARY_PATH=$UCVM_WEB_INSTALL_PATH/lib/euclid3/lib:$UCVM_WEB_INSTALL_PATH/lib/proj-5/lib:$LD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$UCVM_WEB_INSTALL_PATH/lib/euclid3/lib:$UCVM_WEB_INSTALL_PATH/lib/proj-5/lib:$UCVM_WEB_INSTALL_PATH/model/cvmh1511/lib:$UCVM_WEB_INSTALL_PATH/model/cvmsi/lib:$LD_LIBRARY_PATH
   else
-    export LD_LIBRARY_PATH=$UCVM_WEB_INSTALL_PATH/lib/euclid3/lib:$UCVM_WEB_INSTALL_PATH/lib/proj-5/lib
+    export LD_LIBRARY_PATH=$UCVM_WEB_INSTALL_PATH/lib/euclid3/lib:$UCVM_WEB_INSTALL_PATH/lib/proj-5/lib:$UCVM_WEB_INSTALL_PATH/model/cvmh1511/lib:$UCVM_WEB_INSTALL_PATH/model/cvmsi/lib
 fi
 
 ## setup python script
@@ -26,7 +22,7 @@ if [ $PYTHONPATH ] ; then
      export PYTHONPATH=$UCVM_WEB_INSTALL_PATH/utilities/pycvm
 fi
 
-export DYLD_LIBRARY_PATH=$UCVM_WEB_INSTALL_PATH/lib/euclid3/lib:$UCVM_WEB_INSTALL_PATH/lib/proj-5/lib
+export DYLD_LIBRARY_PATH=$UCVM_WEB_INSTALL_PATH/lib/euclid3/lib:$UCVM_WEB_INSTALL_PATH/lib/proj-5/lib:$UCVM_WEB_INSTALL_PATH/model/cvmh1511/lib:$UCVM_WEB_INSTALL_PATH/model/cvmsi/lib
 
 if [ $PATH ] ; then
   export PATH=$UCVM_WEB_INSTALL_PATH/bin:$PATH
