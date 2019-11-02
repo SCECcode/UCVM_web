e9f16df70946abbbaf4e10f086e4e06b63b83623
root <root@ip-172-31-31-151.us-west-2.compute.internal>
2019-11-02 07:27:18 +0000

    anaconda is now at /usr/local/share/anaconda2

--- web/php/util.php
+++ web/php/util.php
@@ -4,7 +4,7 @@ function makeEnvString() {
    if ($myhost == "MeiPro.local") {
       $envstr="PROJ_LIB=/Users/mei/SCEC/anaconda2/share/proj PATH=../model/UCVMC_TARGET/bin:/Users/mei/SCEC/anaconda2/bin:/usr/local/opt/libxml2/bin:/usr/local/opt/sqlite/bin:/usr/local/opt/libxml2/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin PYTHONPATH=../model/UCVMC_TARGET/utilities/pycvm";
    } else { 
-	   $envstr="PROJ_LIB=/usr/local/anaconda2/share/proj PATH=/usr/local/anaconda2/bin:/usr/local/anaconda2/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin PYTHONPATH=../model/UCVMC_TARGET/utilities/pycvm";
+	   $envstr="PROJ_LIB=/usr/local/share/anaconda2/share/proj PATH=/usr/local/share/anaconda2/bin:/usr/local/share/anaconda2/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin PYTHONPATH=../model/UCVMC_TARGET/utilities/pycvm";
    }
    return $envstr;
 }
