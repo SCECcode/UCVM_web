94c1fadec7d896565f3eed817a7780b43e7f0452
Mei-Hui Su <mei@MeiPro.local>
2019-12-12 13:19:24 -0800

    remove GFM references

--- web/contact.php
+++ web/contact.php
@@ -33,7 +33,7 @@ $header = getHeader("Contact");
             The <b>Southern California Earthquake Center</b> (SCEC) was founded as a Science & Technology Center on February 1, 1991, with joint funding by the National Science Foundation (NSF) and the U. S. Geological Survey (USGS). SCEC graduated from the STC Program in 2002 and has been funded as a stand-alone center under cooperative agreements with both agencies in four consecutive phases: SCEC2, 1 Feb 2002 to 31 Jan 2007; SCEC3, 1 Feb 2007 to 31 Jan 2012; SCEC4, 1 Feb 2012 to 31 Jan 2017; and SCEC5, 1 Feb 2017 to 31 Jan 2022.
         </p>
         <p>
-            Please visit <a href="https://www.scec.org/about/">https://www.scec.org/about</a> to learn more about SCEC.  Please email <a href="mailto:software@scec.org">software@scec.org</a> for more information about this GFM prototype.
+            Please visit <a href="https://www.scec.org/about/">https://www.scec.org/about</a> to learn more about SCEC.  Please email <a href="mailto:software@scec.org">software@scec.org</a> for more information.
         </p>
     </div>
 
