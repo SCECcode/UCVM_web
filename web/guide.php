7231899c3c750428fe511ce9e4b8d60fb14a90b0
Mei-Hui Su <mei@MeiPro.local>
2020-09-03 22:30:01 -0700

    add more info into guide.php

--- web/guide.php
+++ web/guide.php
@@ -35,40 +35,47 @@ $header = getHeader("User Guide");
                 <img src="img/ucvm-viewer.png" class="figure-img img-fluid" alt="Screen capture of UCVM Viewer interface">
                 <figcaption class="figure-caption">Screen capture of UCVM Viewer interface</figcaption>
             </figure>
-            <h4>SCEC Community Models Viewer Overview</h4>
-            <p>The CM Viewer provides a map-based access to <a
-                        href="https://www.scec.org/research/ucvm">UCVM version 19.4</a> release. 
+            <h3>SCEC Community Models Viewer Overview</h3>
+            <p>The CM Viewer provides a map-based access to <a href="https://www.scec.org/research/ucvm">UCVM version 19.4</a> release. 
                It allows users to retrieve material properties from different Community Velocity Models. It also allows user to create 1D vertical profiles, 2D vertical cross sections and 2D horizontal slices from retrieved material properties</p>
-            <p>The pages on this site are the <a href="<?php echo $host_site_actual_path; ?>">CM viewer page</a>, this user guide, <a href="disclaimer">a disclaimer</a>, and a <a href="contact">contact information</a> page.</p>
+              <p>The pages on this site are the <a href="<?php echo $host_site_actual_path; ?>">CM viewer page</a>, this user guide, <a href="disclaimer">a disclaimer</a>, and a <a href="contact">contact information</a> page.</p>
 
-            <p>The main interface is on the <a href="<?php echo $host_site_actual_path; ?>">Viewer Page</a>. On top of the map, there are selection buttons for overlay to include fault information from CFM, geological regions from GFM and/or heat flow regions from CTM. </p>
-            <p> Also on top of the map, there is a control that allows the base map to be changed. 
+              <p>The main interface is on the <a href="<?php echo $host_site_actual_path; ?>">Viewer Page</a>. On top of the map, there are selection buttons for overlay to include fault information from CFM, geological regions from GFM and/or heat flow regions from CTM. </p>
+              <p>Also on top of the map, there is a control that allows the base map to be changed. 
                 By default, the map shown is <a href="https://services.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer">ESRI Topographic</a>.
                 The other map types are: <a
                         href="https://services.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer">ESRI National Geographic</a>, <a
                         href="https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer">ESRI Imagery</a>, <a
                         href="https://opentopomap.org">OTM Topographic</a>, and <a href="https://www.openstreetmap.org">OSM Street</a>.</p>
 
-            <p>Both material properties and downloadable results are populated into two tables at the bottom of the interface page. Click on the eye icon (<span class="glyphicon glyphicon-eye-open"></span>) to highlight related location on the map.  To return to non map select mode, return the selection dropdown to 'Select' mode. To return to the initial view, click the "RESET" button. </p>
+              <p>Both material properties and downloadable results are populated into two tables at the bottom of the interface page. Click on the eye icon (<span class="glyphicon glyphicon-eye-open"></span>) to highlight related location on the map.  To return to non map select mode, return the selection dropdown to 'Select' mode. To return to the initial view, click the "RESET" button. </p>
 
-            <h4>Model selection..</h4>
-             <p></p>
-            <h4>Z mode selection..</h4>
-             <p>There are two modes to select from, one is elevation and the other is depth. </p>
-
-            <p>To return to the initial view with no selection, click the "RESET" button.</p>
-            <h4>Material Property Search..</h4>
-            <p> When performing with latitude/longitude search, there are two search methods. The first method is to
+            <h3>Model Selection</h3>
+             <p>A subset of UCVM models are deployed and selectable in the viewer. The boundary of the selected model is displayed on the map. For model details please refer to the UCVM documentation--here.</p>
+            <h3>Z Mode Selection</h3>
+             <p>There are two query modes to select from: by elevation or by depth. Query by elevation is for Z in meters above or below the sea level. An elevation of -100 meter is 100 meter below the sea level and elevation of 200 meter is 200 meter above the sea level.  Query by depth is for Z in meters at or below the surface.  A depth of 0 is at the surface and a depth of 1000 meter is 1000 meter below the surface.</p>
+            <h3>Material Property Search</h3>
+              <p> When performing material property search, there are two location selection methods. The first method is to
                 enter the latitude/longitude values of the location into the text boxes, then clicking 
                 the search icon <span style="white-space: nowrap;">(<span
-                            style="color:#53A2BE;" class="glyphicon glyphicon-search"></span>).</span>
-                The second method is to click on the map to pick a point. </p>
-            <p>User can also upload a local file composed of latlongs and Z values... </p>
-            <p>Downloadable result is placed in the result table at the bottom of the view page </p>
+                            style="color:#990000;font-size:20px" class="glyphicon glyphicon-search"></span>).</span>
+                The second method is to pick a point on the map with a mouse click. The text boxes will be populated with values from the selection. Next, initiate the search by clicking the search icon <span style="white-space: nowrap;">(<span style="color:#990000;font-size:20px" class="glyphicon glyphicon-search"></span>).</span>
+                User can also select a local file to upload composed of latlongs and Z values. Remember to select the right Z mode for the Z values in the file.</p>
 
-            <h4>1D Vertical Profile..</h4>
-            <h4>2D Vertical Cross Section..</h4>
-            <h4>2D Horizontal Slice..</h4>
+            <h3>1D Vertical Profile</h3>
+              <p>
+              Plotting of the depth or elevation vertical profile also uses the same location selection methods used by the material property search. The Z step is the vertical step in meters. 
+              </p>
+            <h3>2D Vertical Cross Section</h3>
+              <p>
+              Plotting of a vertical cross section requires drawing a line on the map by either entering into the text boxes or by clicking a starting and an ending locations on the map. Default starting Z, ending Z and data type can be adjusted to different values.  
+              </p>
+            <h3>2D Horizontal Slice</h3>
+              <p>
+              Plotting of a horizontal slice requires drawing a rectangle on th map by either entering into the text boxes or drag and draw a rectangle on the map. Default Z and data type can be adjusted to different values.
+              </p>
+
+            <p>To return to the initial view with no selection, click the "RESET" button.</p>
 
             <h4>Browser Requirements</h4>
             <p>This site supports the latest versions of <a href="https://www.mozilla.org/en-US/firefox/">Firefox</a>, <a href="https://www.google.com/chrome/">Chrome</a>, <a href="https://www.apple.com/safari/">Safari</a>, and <a href="https://www.microsoft.com/en-us/windows/microsoft-edge">Microsoft Edge</a>.</p>
