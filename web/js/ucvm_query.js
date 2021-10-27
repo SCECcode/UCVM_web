bb189403ccd912265f2ae2ed6198fc9087803555
Mei-Hui Su <mei@MeiPro.local>
2021-10-27 10:12:45 -0700

    test with new basin from Andreas

--- web/js/ucvm_query.js
+++ web/js/ucvm_query.js
@@ -18,6 +18,7 @@ function getInstallModelList() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("phpResponseTxt").innerHTML = this.responseText;
             var str=processSearchResult("getInstallModelList");
+window.console.log("HERE.. "+str);
             makeInstallModelList(str);
        }
     }
